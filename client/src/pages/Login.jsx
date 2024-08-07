757d5ebb7ec6db185a1ef87fccbee2f95d0fe2ee
abishapandian <abishapandiankj@gmail.com>
2024-08-07 13:12:26 +0530

    adding login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,44 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./style/login.css";
-import { useState } from "react";
-export default function login() {
-  const [Username, setusername] = useState("");
-  const [password, setpassword] = useState("");
-  const handlesubmission = (event)=>{
-    event.preventDefault()
-    console.log(Username,password)
-  }
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  const users = [
+   { username: "Sree", password: "2604" },
+   { username: "anu", password: "1234" },
+   { username: "abii", password: "129" }]
+
+  const handleSubmission = (event) => {
+    event.preventDefault();
+    for (let user of users) {
+      if (username === user.username) {
+        if (password === user.password) {
+          navigate("/Home");
+        } else {
+          toast.error("Incorrect password");
+        }
+      } else {
+        toast.error("Incorrect username");
+      }
+    }
+    
+  };
+
   return (
-    <>
-      <div className="login">
-        <h1>LOGIN</h1>
-        <h3>Please Enter Your Login And Passworrd</h3>
-        <div className="form">
-          <form onSubmit={handlesubmission}>
-            <div className="inputs">
-              <input
-                type="text"
-                value={Username}
-                onChange={(event) => setusername(event.target.value)}
-                placeholder="Username"
-                />
-              <br />
-              <input
-                type="password"
-                value={password}
-                onChange={(event) => setpassword(event.target.value)}
-                placeholder="Password"
-              ></input>
-              <br></br>
-            </div>
-            <a className="forgotpassword" href="forgot password">
-              Forgot Password?
-            </a>
-            <br></br>
-            <button type="submit">Login</button>
-          </form>
+    <section id="loginpage">
+      <h1>Welcome, Explore, Enjoy AVV Mart</h1>
+      <h1 className="login">LOGIN</h1>
+      <form onSubmit={handleSubmission}>
+        <div className="username">
+          <h2>Username:</h2>
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            placeholder="Enter Your Username"
+          />
         </div>
-      </div>
-    </>
+        <div className="password">
+          <h2>Password:</h2>
+          <input
+            type="text"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Enter Your Password"
+          />
+        </div>
+        <div className="forgotpassword">
+          <h2>Remember Me</h2>
+          <h2>Forgot password?</h2>
+        </div>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        <div className="button">
+          <button type="submit">Login</button>
+        </div>
+        <ToastContainer />
+      </form>
+    </section>
   );
 }
